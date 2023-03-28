0f86409e415d4225bf84c559091eaaf007bd81c0
hangeul <sheet4252@gmail.com>
2023-03-28 18:03:17 +0900

    shop2 DAO DTO 연습

---
+++ shop/src/shop2/IShop.java
@@ -0,0 +1,11 @@
+package shop2;
+
+//상수, 모든메서드는 추상
+public interface IShop {
+	//public static abstract
+	//=>자동장착되있음
+	void setTitle(String title);
+	void genUser();
+	void genProduct();
+	void start();
+}
