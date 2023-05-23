35307cc02afb7b5d30b3960751abcca0817655d1
hangeul <sheet4252@gmail.com>
2023-05-23 20:11:04 +0900

    web db연동

--- web1/src/main/java/exam/LoginServlet.java
+++ web1/src/main/java/exam/LoginServlet.java
@@ -8,9 +8,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import user.domain.LoginDTO;
 
 @WebServlet("/login")
-public class Login extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -21,6 +24,13 @@ public class Login extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.print("<h4>아이디: "+id+"</h4>");
 		out.print("<h4>비밀번호: "+pwd+"</h4>");
+		
+		// 세션 저장
+		HttpSession session = request.getSession();
+		session.setAttribute("loginDTO", new LoginDTO(id,pwd));
+		
+		// 페이지 이동 코드
+		out.print("<h4><a href=\"session/request2.jsp\">이동</a></h4>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
